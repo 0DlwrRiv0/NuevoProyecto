85cc3a5c86a84520ed05b72645b355cf949cf79a
0DlwrRiv0 <urbano.rivera.delaney@gmail.com>
2023-02-20 21:41:48 -0600

    JAJAJA

--- app/src/main/java/com/example/dlwr/Login.java
+++ app/src/main/java/com/example/dlwr/Login.java
@@ -8,20 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
-    private Button Registrate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Registrate.findViewById(R.id.IniciaID);
-        Registrate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Login.this, Registro.class);
-                startActivity(intent);
-            }
-        });
+
     }
 }
--- app/src/main/java/com/example/dlwr/Registro.java
+++ app/src/main/java/com/example/dlwr/Registro.java
@@ -8,20 +8,10 @@ import android.view.View;
 import android.widget.Button;
 
 public class Registro extends AppCompatActivity {
-    private Button iniciaSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-
-        iniciaSesion = findViewById(R.id.IniciaID);
-        iniciaSesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent inicio = new Intent(Registro.this, Login.class);
-                startActivity(inicio);
-            }
-        });
     }
 }
