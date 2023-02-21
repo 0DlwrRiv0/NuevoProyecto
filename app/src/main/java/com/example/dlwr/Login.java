8c03921783aa77ad6532e566e5c36434c13f837b
0DlwrRiv0 <urbano.rivera.delaney@gmail.com>
2023-02-20 19:19:46 -0600

    AHSJAH

--- app/src/main/java/com/example/dlwr/Login.java
+++ app/src/main/java/com/example/dlwr/Login.java
@@ -2,13 +2,26 @@ package com.example.dlwr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
+    private Button Registrate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Registrate.findViewById(R.id.IniciaID);
+        Registrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Registro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dlwr/Registro.java
+++ app/src/main/java/com/example/dlwr/Registro.java
@@ -2,13 +2,26 @@ package com.example.dlwr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registro extends AppCompatActivity {
+    private Button iniciaSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        iniciaSesion = findViewById(R.id.IniciaID);
+        iniciaSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inicio = new Intent(Registro.this, Login.class);
+                startActivity(inicio);
+            }
+        });
     }
 }
